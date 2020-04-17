46c95a831be079f622fcafa9236e6c33bb6cd7a8
oliverpdahl <oliverpdahl@gmail.com>
2020-04-17 19:04:52 -0400

    Create create method

--- spaced-repetition-backend/app/controllers/memories_controller.rb
+++ spaced-repetition-backend/app/controllers/memories_controller.rb
@@ -2,4 +2,9 @@ class MemoriesController < ApplicationController
   def index
     render json: Memory.all
   end
+
+  def create 
+    memory = Memory.create(title: params['title'])
+    render json: memory
+  end
 end
