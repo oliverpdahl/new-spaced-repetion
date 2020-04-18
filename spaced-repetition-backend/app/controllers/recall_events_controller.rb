d97ed0bce580e3c7814e1d569bf4667b79b742db
oliverpdahl <oliverpdahl@gmail.com>
2020-04-18 16:02:00 -0400

    add update for checked

--- spaced-repetition-backend/app/controllers/recall_events_controller.rb
+++ spaced-repetition-backend/app/controllers/recall_events_controller.rb
@@ -1,2 +1,6 @@
 class RecallEventsController < ApplicationController
+  def update
+    new_status = {id: params[:id], complete: params[:complete]}
+    RecallEvent.find(params[:id]).update(new_status)
+  end
 end
