d1f5d07c40a677fac38dfd26a20e3706690ef77a
oliverpdahl <oliverpdahl@gmail.com>
2020-04-18 16:51:34 -0400

    Make scheduled date attribute for recall event

--- spaced-repetition-backend/app/models/recall_event.rb
+++ spaced-repetition-backend/app/models/recall_event.rb
@@ -1,3 +1,8 @@
 class RecallEvent < ApplicationRecord
   belongs_to :memory
+  class_attribute :scheduled_date, default: set_scheduled_date
+
+  def set_scheduled_date
+    memory.start_date + daysDistant
+  end
 end
