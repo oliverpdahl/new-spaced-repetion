281ad1c9e82ddd77903ae0a3c8711f51f6f23246
oliverpdahl <oliverpdahl@gmail.com>
2020-04-18 19:01:55 -0400

    Add category and strategy to memory

--- spaced-repetition-backend/db/seeds.rb
+++ spaced-repetition-backend/db/seeds.rb
@@ -5,12 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-memories = [{ title:'Factfullness Book', start_date: (Date.today - 50)}, 
-            {title: 'Visual Studio Code Shortcuts', start_date: (Date.today - 25)}, 
-            {title: 'Central American Capitals', start_date: (Date.today - 1)}]
+memories = [{ title:'Factfullness Book', 
+              start_date: (Date.today - 50),
+              category: 'Reading',
+              strategy: 'Write down the three most important points and then review reading notes'
+            }, 
+            { title: 'Visual Studio Code Shortcuts', 
+              start_date: (Date.today - 25),
+              category: 'Shortcuts',
+              strategy: `Go through list and first try the action before looking at the command. Practice the ones where you didn't know the command three times`
+            }, 
+            { title: 'Central American Capitals', 
+              start_date: (Date.today - 1),
+              category: 'Geography',
+              strategy: `Go through quizlet until you get all of them right`
+            }]
 
 
 memories.each do |mem|
-  memory = Memory.create(title: mem[:title], start_date: mem[:start_date])
+  memory = Memory.create(title: mem[:title], start_date: mem[:start_date], category: mem[:category], strategy: mem[:strategy])
   memory.create_recall_events
 end
