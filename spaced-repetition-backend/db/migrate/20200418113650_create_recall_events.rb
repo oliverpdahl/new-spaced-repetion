1e1ab27f00159ff5dc0c4522dce5de3a849b7772
oliverpdahl <oliverpdahl@gmail.com>
2020-04-18 15:56:18 -0400

    Add complete to recall events

--- spaced-repetition-backend/db/migrate/20200418113650_create_recall_events.rb
+++ spaced-repetition-backend/db/migrate/20200418113650_create_recall_events.rb
@@ -3,6 +3,8 @@ class CreateRecallEvents < ActiveRecord::Migration[6.0]
     create_table :recall_events do |t|
       t.references :memory, null: false, foreign_key: true
       t.integer :daysDistant
+      t.boolean :complete, default: false
+      
       t.timestamps
     end
   end
