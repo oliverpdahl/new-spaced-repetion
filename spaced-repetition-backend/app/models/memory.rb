cc33faa4e8a0d0b43769732a6376702b4ca33ee2
oliverpdahl <oliverpdahl@gmail.com>
2020-04-18 12:19:03 -0400

    Added schedules to memory

--- spaced-repetition-backend/app/models/memory.rb
+++ spaced-repetition-backend/app/models/memory.rb
@@ -1,7 +1,15 @@
 class Memory < ApplicationRecord
   has_many :recall_events, dependent: :destroy
 
+  SHEDULES = {
+    superMemo: [1, 7, 16, 35]
+  }.freeze
+
   def create_recall_events
-    self.recall_events << RecallEvent.create
+    recall_events << RecallEvent.create
+  end
+
+  def self.schedules 
+    return SHEDULES
   end
 end
