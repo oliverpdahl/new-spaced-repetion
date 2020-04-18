2e929dff21a61bbba08a4156c1036fd3a4a0da76
oliverpdahl <oliverpdahl@gmail.com>
2020-04-18 18:54:27 -0400

    Add strategy, category, and description to Memory

--- spaced-repetition-backend/db/migrate/20200417143719_create_memories.rb
+++ spaced-repetition-backend/db/migrate/20200417143719_create_memories.rb
@@ -4,6 +4,10 @@ class CreateMemories < ActiveRecord::Migration[6.0]
       t.string :title
       t.string :scheduleKey, default: Memory.first_key_of_schedules
       t.date :start_date, default: Date.today
+      t.string :strategy
+      t.string :category
+      t.string :description
+      
       t.timestamps
     end
   end
