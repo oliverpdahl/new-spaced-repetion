ed64adc7ee41eb02abccd05d5be193dd7effb71b
oliverpdahl <oliverpdahl@gmail.com>
2020-04-18 17:02:10 -0400

    Set scheduled date after creation

--- spaced-repetition-backend/app/models/recall_event.rb
+++ spaced-repetition-backend/app/models/recall_event.rb
@@ -1,8 +1,8 @@
 class RecallEvent < ApplicationRecord
   belongs_to :memory
-  class_attribute :scheduled_date, default: set_scheduled_date
+  after_create :set_scheduled_date
 
   def set_scheduled_date
-    memory.start_date + daysDistant
+    scheduled_date = memory.start_date + daysDistant
   end
 end
